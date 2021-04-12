700f828878f1dbd2d654de601760c36033f8f5fe
BenZoN <s.benzon@gmail.com>
2021-04-12 15:48:18 +0200

    Danish translation, added catch to avoid error spam if msg is deleted.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout =
 	if (!pages) throw new Error('Pages are not given.');
 	if (emojiList.length !== 2) throw new Error('Need two emojis.');
 	let page = 0;
-	const curPage = await msg.channel.send(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+	const curPage = await msg.channel.send(pages[page].setFooter('-------------------------------------------------- ' + `Side ${page + 1} / ${pages.length}`  + ' --------------------------------------------------')).catch(function (e) { });
 	for (const emoji of emojiList) await curPage.react(emoji);
 	const reactionCollector = curPage.createReactionCollector(
 		(reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot,
 		{ time: timeout }
 	);
 	reactionCollector.on('collect', reaction => {
-		reaction.users.remove(msg.author);
+		reaction.users.remove(msg.author).catch(function (e) { });
 		switch (reaction.emoji.name) {
 			case emojiList[0]:
 				page = page > 0 ? --page : pages.length - 1;
@@ -21,11 +21,11 @@ const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout =
 			default:
 				break;
 		}
-		curPage.edit(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+		curPage.edit(pages[page].setFooter('-------------------------------------------------- ' + `Side ${page + 1} / ${pages.length}` + ' --------------------------------------------------')).catch(function (e) { });
 	});
 	reactionCollector.on('end', () => {
 		if (!curPage.deleted) {
-			curPage.reactions.removeAll()
+			curPage.reactions.removeAll().catch(function (e) { })
 		}
 	});
 	return curPage;
