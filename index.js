fde31c1152527fc695a98f8fe9e8400531e43dea
BenZoN <s.benzon@gmail.com>
2021-05-03 17:08:25 +0200

    i18n integration (Dont use this fork if you dont know what it dos)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const i18n = require('i18n');
+
 const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout = 120000) => {
-	if (!msg && !msg.channel) throw new Error('Channel is inaccessible.');
-	if (!pages) throw new Error('Pages are not given.');
-	if (emojiList.length !== 2) throw new Error('Need two emojis.');
+	if (!msg && !msg.channel) throw new Error(i18n.__('Channel is inaccessible.'));
+	if (!pages) throw new Error(i18n.__('Pages are not given.'));
+	if (emojiList.length !== 2) throw new Error(i18n.__('Need two emojis.'));
 	let page = 0;
-	const curPage = await msg.channel.send(pages[page].setFooter('-------------------------------------------------- ' + `Side ${page + 1} / ${pages.length}`  + ' --------------------------------------------------')).catch(function (e) { });
+	const curPage = await msg.channel.send(pages[page].setFooter(i18n.__('-------------------------------------------------- Page %s / %s --------------------------------------------------', page + 1, pages.length))).catch(function (e) { });
 	for (const emoji of emojiList) await curPage.react(emoji);
 	const reactionCollector = curPage.createReactionCollector(
 		(reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot,
@@ -21,7 +23,7 @@ const paginationEmbed = async (msg, pages, emojiList = ['⏪', '⏩'], timeout =
 			default:
 				break;
 		}
-		curPage.edit(pages[page].setFooter('-------------------------------------------------- ' + `Side ${page + 1} / ${pages.length}` + ' --------------------------------------------------')).catch(function (e) { });
+		curPage.edit(pages[page].setFooter(i18n.__('-------------------------------------------------- Page %s / %s --------------------------------------------------', page + 1, pages.length))).catch(function (e) { });
 	});
 	reactionCollector.on('end', () => {
 		if (!curPage.deleted) {
